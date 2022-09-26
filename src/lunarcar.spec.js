4e0e85049a60dd4cda34833e4d9cec01d0e8e668
santytorrico <90476969+santytorrico@users.noreply.github.com>
2022-09-26 16:57:16 -0400

    roadster can go forward once in the same direction an return its position

--- src/lunarcar.spec.js
+++ src/lunarcar.spec.js
@@ -6,4 +6,9 @@ describe("Movement", () => {
     it("Return actual position", () => {
       expect(roadster.execute(" ")).toEqual("0,0N");
     });
+
+    it("Go forward once on the same direction and Return actual position", () => {
+        expect(roadster.execute("A")).toEqual("0,1N");
+      });
+
   });
